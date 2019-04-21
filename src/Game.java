66b64b218c975c366aba68ecc754e4c44a69adbd
Vasi <karolina21@mail.bg>
2019-04-21 18:12:55 +0300

    CreateMap

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,13 @@
-public class Game {
+import javax.swing.*;
+
+public class Game extends JFrame {
+
+    public Game() {
+        super.setTitle("Car Race");
+        super.setBounds(0, 0, 1950, 1035);
+        super.add(new Map());
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        super.setVisible(true);
+
+    }
 }
