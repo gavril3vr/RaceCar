a454daa1f9df9352520eb3e8f4a8936b715e7ddc
Vasi <karolina21@mail.bg>
2019-04-23 00:45:51 +0300

    addLimit

--- src/Game.java
+++ src/Game.java
@@ -5,9 +5,8 @@ public class Game extends JFrame {
     public Game() {
         super.setTitle("Car Race");
         super.setBounds(0, 0, 1950, 1035);
-        super.add(new Map());
+        super.add(new Car());
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         super.setVisible(true);
-
     }
 }
